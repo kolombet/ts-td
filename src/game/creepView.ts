428d01c6e9c0f3700b5e89c9b87d84eb0b5608e1
kolombet <kkolombet@gmail.com>
2018-07-02 10:54:38 +0300

    creep view

--- src/game/creepView.ts
+++ src/game/creepView.ts
@@ -1,41 +1,43 @@
-import Starling from "starling/core/Starling";
 import Sprite from "starling/display/Sprite";
 import { IGameView, IGameObj } from "./interfaces";
 import BaseCreepData from "./baseCreepData";
-import Direction from "./direction";
-import MovieClip from "starling/display/MovieClip";
-import Quad from "starling-framework/lib/starling/display/Quad";
+import Quad from "starling/display/Quad";
+import Image from "starling/display/Image";
 import KPoint from "./KPoint";
 import IsoTransform from "./isoTransform";
-import Pools from "./pools";
 import App from "./app";
 import { Guid } from "guid-typescript";
 import Config from "./config";
 
 export default class CreepView extends Sprite implements IGameView {
-    private static GNOME: string = "gnome";
-    private static ORK: string = "ork";
     private _creepData: BaseCreepData;
-    private _animations: Object;//<MovieClip>
-    private _animationsList: MovieClip[];
-    private _playingAnimation: string;
-    private _mName: string;
+    // private _animations: Object;//<MovieClip>
+    // private _animationsList: MovieClip[];
+    // private _playingAnimation: string;
     private _uid: string;
 
     constructor(data: BaseCreepData) {
         super();
         this._uid = Guid.create().toString();
-        this._mName = (Math.random() > .5) ? CreepView.GNOME : CreepView.ORK;
         this._creepData = data;
-        this._animations = {};
-        this._animationsList = [];
 
-        let animations: any[] = Direction.getList();
-        for (let i: number = 0; i < animations.length; i++) {
-            this.createAnimation(animations[i]);
-        }
-        this.playAnimation("TL");
-        if (Config.DEBUG_CREEP_CENTER)
+        // const img = new Image(App.resources.assets.getTexture("creep"));
+        // img.x = -img.width/2;
+        // img.y = -img.height/2;
+        // this.addChild(img);
+
+
+        // this._animations = {};
+        // this._animationsList = [];
+
+        // let animations: any[] = Direction.getList();
+        // for (let i: number = 0; i < animations.length; i++) {
+        //     this.createAnimation(animations[i]);
+        // }
+        // this.playAnimation("TL");
+
+
+        // if (Config.DEBUG_CREEP_CENTER)
             this.drawFooting();
     }
 
@@ -43,36 +45,35 @@ export default class CreepView extends Sprite implements IGameView {
         return this._uid;
     }
 
-    private playAnimation(type: string): void {
-        if (this._playingAnimation) {
-            let prevAnim: MovieClip = this._animations[this._playingAnimation];
-            prevAnim.stop();
-            prevAnim.visible = false;
-            Starling.current.juggler.remove(prevAnim);
-        }
-
-        let anim: MovieClip = this._animations[type];
-        anim.visible = true;
-        anim.play();
-        Starling.current.juggler.add(anim);
-        this._playingAnimation = type;
-    }
+    // private playAnimation(type: string): void {
+    //     if (this._playingAnimation) {
+    //         let prevAnim: MovieClip = this._animations[this._playingAnimation];
+    //         prevAnim.stop();
+    //         prevAnim.visible = false;
+    //         Starling.current.juggler.remove(prevAnim);
+    //     }
+    //
+    //     let anim: MovieClip = this._animations[type];
+    //     anim.visible = true;
+    //     anim.play();
+    //     Starling.current.juggler.add(anim);
+    //     this._playingAnimation = type;
+    // }
 
     private hideAll(): void {
-        this._animationsList.forEach((animation: MovieClip) => {
-            animation.stop();
-            animation.visible = false;
-            Starling.current.juggler.remove(animation);
-        });
+        // this._animationsList.forEach((animation: MovieClip) => {
+        //     animation.stop();
+        //     animation.visible = false;
+        //     Starling.current.juggler.remove(animation);
+        // });
     }
 
-    public createAnimation(type: string): void {
-        let animation: MovieClip = App.resources.getMonsterAnimation(this._mName, type);
-        animation.visible = false;
-        this.addChild(animation);
-        this._animations[type] = animation;
-        //console.log("create animation")
-    }
+    // public createAnimation(type: string): void {
+    //     let animation: MovieClip = App.resources.getMonsterAnimation(this._mName, type);
+    //     animation.visible = false;
+    //     this.addChild(animation);
+    //     this._animations[type] = animation;
+    // }
 
     public drawFooting(): void {
         let footing: Quad = new Quad(4, 4, 0x00ff00);
@@ -84,10 +85,10 @@ export default class CreepView extends Sprite implements IGameView {
     public destroy(): void {
         this._creepData = null;
 
-        this._animationsList.forEach((animation: MovieClip) => {
-            this.removeChild(animation);
-            animation.dispose();
-        });
+        // this._animationsList.forEach((animation: MovieClip) => {
+        //     this.removeChild(animation);
+        //     animation.dispose();
+        // });
         super.dispose();
     }
 
@@ -97,15 +98,14 @@ export default class CreepView extends Sprite implements IGameView {
             //console.log("creep data null");
         }
 
-        if (this._creepData.rotationName && this._playingAnimation != this._creepData.rotationName) {
-            this.playAnimation(this._creepData.rotationName);
-        }
+        // if (this._creepData.rotationName && this._playingAnimation != this._creepData.rotationName) {
+        //     this.playAnimation(this._creepData.rotationName);
+        // }
         let creepPoint = new KPoint(this._creepData.x, this._creepData.y);
         let p: KPoint = IsoTransform.fromP(creepPoint);
-        this.x = p.x;
-        this.y = p.y;
+        this.x = Math.round(p.x);
+        this.y = Math.round(p.y);
 
-        Pools.point.object = p;
     }
 
     public getData(): IGameObj {
